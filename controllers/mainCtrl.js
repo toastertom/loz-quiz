5ecacee18f3e8149a3a08e8702083e7436e0a253
ToasterTom <tate.thomas5595@gmail.com>
2017-01-23 22:43:34 -0700

    Fully progress through the quiz and progress bar is fully functioning

--- controllers/mainCtrl.js
+++ controllers/mainCtrl.js
@@ -9,4 +9,37 @@ angular.module('lozQuiz').controller('mainCtrl', function ($scope, $http, $sce) 
     $scope.myQuestions = quizData.data;
     $scope.totalQuestions = $scope.myQuestions.length;
   });
+
+  $scope.selectAnswer = function (qIndex, aIndex) {
+    var questionState = $scope.myQuestions[qIndex].questionState;
+
+    if (questionState != 'answered') {
+      $scope.myQuestions[qIndex].selectedAnswer = aIndex;
+      var correctAnswer = $scope.myQuestions[qIndex].correct;
+      $scope.myQuestions[qIndex].correctAnswer = correctAnswer;
+
+      if (aIndex === correctAnswer) {
+        $scope.myQuestions[qIndex].correctness = 'correct';
+        $scope.score += 1;
+      }else{
+        $scope.myQuestions[qIndex].correctness = 'incorrect';
+      };
+
+    $scope.myQuestions[qIndex].questionState = 'answered';
+
+    };
+  };
+
+  $scope.isSelected = function (qIndex, aIndex) {
+    return $scope.myQuestions[qIndex].selectedAnswer === aIndex;
+  }
+
+  $scope.isCorrect = function (qIndex, aIndex) {
+    return $scope.myQuestions[qIndex].correctAnswer === aIndex;
+  }
+
+  $scope.selectContinue = function () {
+    return $scope.activeQuestion += 1;
+  }
+
 });
