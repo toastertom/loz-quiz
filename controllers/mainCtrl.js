fe851ee2286e2db410ca0c3f5f25407c8eec1f74
ToasterTom <tate.thomas5595@gmail.com>
2017-01-23 20:54:34 -0700

    Pulling json data into the angular appand are able to display it on the dom

--- controllers/mainCtrl.js
+++ controllers/mainCtrl.js
@@ -1,3 +1,12 @@
 angular.module('lozQuiz').controller('mainCtrl', function ($scope, $http, $sce) {
 
+  $scope.score = 0;
+  $scope.activeQuestion = -1;
+  $scope.activeQuestionAnswered = 0;
+  $scope.percentage = 0;
+
+  $http.get('quiz_data.json').then(function (quizData) {
+    $scope.myQuestions = quizData.data;
+    $scope.totalQuestions = $scope.myQuestions.length;
+  });
 });
